1148a3bc8dc6be166be626d527e47a045e214769
Anthony C <broac@live.ca>
2016-02-13 22:10:04 -0800

    Added in text for score and win

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
 
+    private Rigidbody rb;
+    private int count;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -24,7 +31,15 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            ++count;
+            setCountText();
         }
+    }
+
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+            winText.text = "You Win!";
     }
 
 }

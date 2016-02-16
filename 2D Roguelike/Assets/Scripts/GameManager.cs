df39b2f0c9eae74749fafab17ec2ae4058189c4d
Anthony C <broac@live.ca>
2016-02-15 23:24:26 -0800

    Added GameManager and Loader scripts

--- 2D Roguelike/Assets/Scripts/GameManager.cs
+++ 2D Roguelike/Assets/Scripts/GameManager.cs
@@ -1,98 +1,34 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections.Generic;
-using Random = UnityEngine.Random;
+using System.Collections;
 
-public class GameManager : MonoBehaviour {
-    [Serializable]
-    public class Count
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+    // Use this for initialization
+    void Awake()
     {
-        public int minimum;
-        public int maximum;
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
 
-        public Count(int min, int max)
-        {
-            minimum = min;
-            maximum = max;
-        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
     }
 
-    public int columns = 8;
-    public int rows = 8;
-    public Count wallCount = new Count(5, 9);
-    public Count foodCount = new Count(1, 5);
-    public GameObject exit;
-    public GameObject[] floorTiles;
-    public GameObject[] wallTiles;
-    public GameObject[] foodTiles;
-    public GameObject[] EnemyTiles;
-    public GameObject[] outerWallTiles;
-
-    private Transform boardHolder;
-    private List<Vector3> gridPositions = new List<Vector3>();
-
-    void InitialiseList()
+    void InitGame()
     {
-        gridPositions.Clear();
-
-        for (int x = 1; x < columns - 1; x++)
-        {
-            for (int y = 1; y < rows - 1; y++)
-            {
-                gridPositions.Add(new Vector3(x, y, 0f));
-            }
-        }
+        boardScript.SetupScene(level);
     }
 
-    void BoardSetup()
+
+    // Update is called once per frame
+    void Update()
     {
-        boardHolder = new GameObject("Board").transform;
-
-        for (int x = -1; x < columns + 1; x++)
-        {
-            for (int y = -1; y < rows + 1; y++)
-            {
-                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
-                if (x == -1 || x == columns || y == 01 || y == rows)
-                    toInstantiate = outerWallTiles[Random.Range(0, floorTiles.Length)];
-
-                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
-
-                instance.transform.SetParent(boardHolder);
-            }
-        }
-    }
-
-    Vector3 RandomPosition()
-    {
-        int randomIndex = Random.Range(0, gridPositions.Count);
-        Vector3 randomPosition = gridPositions[randomIndex];
-        gridPositions.RemoveAt(randomIndex);
-        return randomPosition;
-    }
-
-    void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max)
-    {
-        int objectCount = Random.Range(min, max + 1);
-
-        for (int i = 0; i < objectCount; i++)
-        {
-            Vector3 randomPosition = RandomPosition();
-            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-            Instantiate(tileChoice, randomPosition, Quaternion.identity);
-        }
-
-    }
-
-    public void SetupScene(int level)
-    {
-        BoardSetup();
-        InitialiseList();
-        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
-        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
-        int enemyCount = (int)Mathf.Log(level, 2f);
-        LayoutObjectAtRandom(EnemyTiles, enemyCount, enemyCount);
-        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
 
     }
 }
